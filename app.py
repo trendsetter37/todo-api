d19f764402c7c35ca432a30f3d2876339c6a4a58
Javis Sullivan <javissullivan@gmail.com>
2015-09-09 16:19:16 -0400

    sqlite issues

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, jsonify
 import sqlite3
 
 app = Flask(__name__)
-conn = sqlite3.connect('tasks.db')
-db = conn.cursor()
+#conn = sqlite3.connect('tasks.db')
+#db = conn.cursor()
 
 @app.route('/todo/api/v1.0/tasks')
 def get_tasks():
+  conn = sqlite3.connect('tasks.db')
+  db = conn.cursor() 
   rows = db.execute('''SELECT * FROM tasks''')
+  conn.close()
   return jsonify({'tasks':rows.fetchall()})
 
 if __name__ == '__main__':
