e080d9dab8689ac2d8e66a2f949e6677978b4481
Javis Sullivan <javissullivan@gmail.com>
2015-09-10 18:45:51 -0400

    added task creation functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, g, make_response
+from flask import Flask, jsonify, abort, g, make_response, request
 from db_tools import connect_to_database
 
 app = Flask(__name__)
@@ -25,18 +25,35 @@ def get_tasks():
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-	db = get_db().cursor()
-	rows = db.execute('''SELECT * FROM tasks WHERE ID=(?)''',(task_id,)).fetchall()
-	if len(rows) == 0:
-		abort(404)
-	return jsonify({'task':rows[0]})
+  db = get_db().cursor()
+  rows = db.execute('''SELECT * FROM tasks WHERE ID=(?)''',(task_id,)).fetchall()
+  if len(rows) == 0:
+	abort(404)
+  return jsonify({'task':rows[0]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+  ''' receiving json from client '''
+  if not request.json or not 'title' in request.json:
+    abort(400)
+  db = get_db()
+  sql = ''' INSERT INTO tasks (title, description, done) VALUES (?,?,?) '''
+  insert = (request.json.get('title'), request.json.get('description', ''),request.json.get('done', 0))
+  db.cursor().execute(sql, insert)
+  db.commit()
+  db.close()
+  return jsonify({'status': 'Task Added'}), 201
 
 ################
 #Error Handlers#
 ################
 @app.errorhandler(404)
 def not_found(error):
-	return make_response(jsonify({'error':'Task Not Found'}), 404)
+  return make_response(jsonify({'error':'Task Not Found'}), 404)
+
+@app.errorhandler(400)
+def malformed(error):
+  return make_response(jsonify({'error':'Malformed json object'}), 400)
 
 if __name__ == '__main__':
   app.run(debug=True)
